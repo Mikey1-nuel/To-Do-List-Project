2d12d9558063133dde93a92f4137b34f3d1fa3d0
Emmanuel Nwoye <emmanuelnwoye12@gmail.com>
2023-04-05 16:21:03 +0100

    Revert "To Do list: list structure"

--- src/modules/delete-all-list.js
+++
@@ -1,9 +0,0 @@
-let collection = JSON.parse(localStorage.getItem('taskList')) || [];
-
-const deleteItems = () => {
-  const deleteAll = collection.filter((item) => item.completed === false);
-  collection = deleteAll;
-  localStorage.setItem('taskList', JSON.stringify(collection));
-  window.location.reload();
-};
-export default deleteItems;
